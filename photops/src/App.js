ecd6d4293a0cd1bbd044683077a3f476606a1315
benjamesmiller <benjamesmiller@gmail.com>
2018-06-02 20:11:33 -0400

    Added logo design and connected to jquery

--- photops/src/App.js
+++ photops/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import request from 'superagent';
+import logo from '/logo.png';
+import jquery from 'jQuery';
+
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      
+      searchtext : '',
+      resultsarray : []
     }
+  }
+
+  displayArray(resultsarray, event) {
+
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <strong className="App-title">Photops</strong>
+          <div className="App-logo">${logo}</div>
           <div class="input-group">
               <input type="text"/>
               <button type="button" class="button-light">Search</button>
